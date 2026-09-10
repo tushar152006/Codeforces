b8907fcff023cb3057b46d049d1348e351a8ea31
Tushar Burla <tusharburla7@gmail.com>
2026-09-11 03:02:43 +0530

    Day 1-> solve 236A and 158A

---
+++ Day-001/158A_NextRound/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+ 
+        int[] arr = new int[n];
+ 
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        m = arr[m - 1];
+ 
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= m && arr[i] > 0) {
+                cnt++;
+            }
+        }
+ 
+        System.out.println(cnt);
+ 
+        sc.close();
+    }
+}
