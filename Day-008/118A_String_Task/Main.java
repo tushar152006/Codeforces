be44dfdea33e730b86027cb6581235d3a3646ea0
Tushar Burla <tusharburla7@gmail.com>
2026-09-24 23:50:42 +0530

    Add Day 8 Codeforces solutions

---
+++ Day-008/118A_String_Task/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next().toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch != 'a' && ch != 'e' && ch != 'i'
+                    && ch != 'o' && ch != 'u' && ch != 'y') {
+                System.out.print("." + ch);
+            }
+        }
+    }
+}
